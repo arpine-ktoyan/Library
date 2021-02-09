f795e81af813b205a603e3d263928c3fd64e9b43
Arpine Ktoyan <arpine.ktoyan.fd@gmail.com>
2021-02-09 09:15:44 +0400

    Delete comments

--- bookDirectory/server/server.js
+++ bookDirectory/server/server.js
@@ -1,8 +1,3 @@
-// Copyright IBM Corp. 2016,2019. All Rights Reserved.
-// Node module: loopback-workspace
-// This file is licensed under the MIT License.
-// License text available at https://opensource.org/licenses/MIT
-
 'use strict';
 require('dotenv').config();
 
@@ -12,7 +7,6 @@ const boot = require('loopback-boot');
 const app = module.exports = loopback();
 
 app.start = function() {
-  // start the web server
   return app.listen(function() {
     app.emit('started');
     const baseUrl = app.get('url').replace(/\/$/, '');
@@ -24,12 +18,9 @@ app.start = function() {
   });
 };
 
-// Bootstrap the application, configure models, datasources and middleware.
-// Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname, function(err) {
   if (err) throw err;
 
-  // start the server if `$ node server.js`
   if (require.main === module)
     app.start();
 });
